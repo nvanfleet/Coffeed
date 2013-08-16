8e053726b4d02eb9b55029a927947c48088f096c
Nathan Van Fleet <nathan.vanfleet@gmail.com>
2013-08-16 10:56:12 -0400

    adding file changes

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,3 +1,8 @@
+// Copyright (C) 2013 Nathan Van Fleet
+//
+// This is free software, licensed under the GNU General Public License v2.
+// See /LICENSE for more information.
+
 //
 //  server.c
 //  CoffeeD
